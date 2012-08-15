d50b354570a05ef94751993c8e7e359a558a32aa
JakobKallin <jakobkallin>
2012-08-15 17:25:37 +0200

    Refactored view models

--- source/immersive/immersive.js
+++ source/immersive/immersive.js
@@ -22,70 +22,7 @@ var ViewModel = function(editorWidth) {
 		self.message(null);
 	};
 	
-	self.scenes = ko.observableArray();
-	
-	var baseScene = {
-		name: 'Untitled',
-		key: '',
-		layer: 'background',
-		mixin: false,
-		image: '',
-		loop: true,
-		shuffle: false,
-		volume: 1,
-		text: '',
-		backgroundColor: '#000000',
-		size: 'contain',
-		fadeDuration: 0,
-		crossoverSeconds: 0,
-		crossfade: false,
-		fontSize: 5,
-		fontFamily: '',
-		fontColor: '#ffffff',
-		bold: false,
-		italic: false,
-		get imageCss() {
-			return 'url("' + this.image + '")';
-		},
-		get isSelected() {
-			return this === self.current();
-		},
-		get previewFontSize() {
-			return (this.fontSize / 100) * 10 + 'em';
-		},
-		get fontStyle() {
-			if ( this.italic ) {
-				return 'italic';
-			} else {
-				return 'normal';
-			}
-		},
-		get fontWeight() {
-			if ( this.bold ) {
-				return 'bold';
-			} else {
-				return 'normal';
-			}
-		},
-		get soundString() {
-			var soundPaths = this.sounds.map(function(sound) {
-				return sound.path;
-			});
-			
-			return soundPaths.join(', ');
-		}
-	};
-	
-	self.createScene = function() {
-		var scene = Object.create(baseScene);
-		scene.sounds = []; // Defined here because each scene needs its own list, not that of its prototype.
-		self.wrapScene(scene);
-		return scene;
-	};
-	
-	self.wrapScene = function(scene) {
-		knockwrap.wrapObject(scene);
-	};
+	self.adventure = new AdventureViewModel(self);
 	
 	self.playScene = function(scene) {
 		var flatScene = new Ambience.Scene();
@@ -121,101 +58,6 @@ var ViewModel = function(editorWidth) {
 		flatScene.isMixin = scene.mixin;
 		
 		ambience.play(flatScene);
-	};
-	
-	self.fadeOutTopmost = function() {
-		ambience.fadeOutTopmost();
-	};
-	
-	self.current = ko.observable();
-	
-	var selectedTab = ko.observable(0);
-	self.select = function(scene) {
-		self.current(scene);
-		var specificOptions = $('.selected-item .options.specific');
-		specificOptions.tabs({
-			select: function(event, ui) {
-				selectedTab(ui.index);
-			}
-		});
-		specificOptions.tabs('select', selectedTab());
-		splitter.update();
-	};
-	
-	self.add = function() {
-		self.scenes.push(self.createScene());
-		self.select(self.last());
-	};
-	
-	self.previous = function() {
-		var index = self.scenes.indexOf(self.current());
-		if ( index > 0 ) {
-			return self.scenes()[index - 1];
-		} else {
-			return null;
-		}
-	};
-	
-	self.next = function() {
-		var index = self.scenes.indexOf(self.current());
-		if ( index < self.scenes().length - 1 ) {
-			return self.scenes()[index + 1];
-		} else {
-			return null;
-		}
-	};
-	
-	self.last = function() {
-		var index = self.scenes().length - 1;
-		if ( index !== -1 ) {
-			return self.scenes()[index];
-		} else {
-			return null;
-		}
-	};
-	
-	self.removeSelected = function() {
-		var previous = self.previous();
-		var next = self.next();
-		
-		if ( self.previous() ) {
-			self.select(previous);
-		} else if ( next ) {
-			self.select(next);
-		} else {
-			self.add();
-		}
-		
-		var index = viewModel.scenes.indexOf(this);
-		self.scenes.splice(index, 1);
-	};
-	
-	self.playSelected = function() {
-		self.playScene(self.current());
-	};
-	
-	self.copySelected = function() {
-		var newScene = self.createScene();
-		for ( var property in this ) {
-			if ( !(this[property] instanceof Object) ) {
-				newScene[property] = this[property];
-			}
-		}
-		this.sounds.map(function(sound) {
-			newScene.sounds.push(Object.copy(sound));
-		});
-		
-		var index = self.scenes.indexOf(self.current()) + 1
-		self.scenes.splice(index, 0, newScene);
-		self.select(newScene);
-	};
-	
-	self.addSound = function() {
-		this.sounds.push({ path: '' });
-	};
-	
-	self.removeSound = function() {
-		self.current().sounds.remove(this);
 	};
 	
 	self.hideEditor = function() {
@@ -266,34 +108,6 @@ var ViewModel = function(editorWidth) {
 		theater.style.cursor = 'auto';
 		self.interfaceIsVisible(true);
 	}
-	
-	var bindableKeys = ['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12'];
-	var specialKeyFound = false;
-	self.bindSpecialKey = function(scene, event) {
-		var keyName = Key.name(event.keyCode);
-		if ( bindableKeys.contains(keyName) ) {
-			specialKeyFound = true;
-			var keyHasCommand = keyName in commands;
-			if ( !keyHasCommand ) {
-				scene.key = keyName;
-			}
-		} else {
-			return true;
-		}
-	};
-	
-	self.bindTextKey = function(scene, event) {
-		if ( !specialKeyFound ) {
-			var keyText = String.fromCharCode(event.which);
-			if ( keyText ) {
-				scene.key = keyText.toUpperCase();
-			}
-		}
-	};
-	
-	self.stopKeyBinding = function(scene, event) {
-		specialKeyFound = false;
-	};
 	
 	self.keyedScene = function(targetKey) {
 		if ( targetKey ) {
@@ -393,5 +207,5 @@ window.addEventListener('load', function() {
 	document.addEventListener('keypress', viewModel.onKeyPress);
 	document.addEventListener('keydown', viewModel.onKeyDown);
 	ko.applyBindings(viewModel);
-	viewModel.add();
+	viewModel.adventure.add();
 });

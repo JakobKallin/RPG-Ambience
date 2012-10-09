3f6950a3305ecd6976944d09e9ebfb764a2ad6bf
JakobKallin <jakobkallin>
2012-10-09 13:36:04 +0200

    No instructions

--- source/immersive/immersive.js
+++ source/immersive/immersive.js
@@ -153,11 +153,6 @@ var ViewModel = function(editorWidth) {
 		self.message(null);
 	};
 	
-	self.instructionsAreVisible = ko.observable(true);
-	self.hideInstructions = function() {
-		self.instructionsAreVisible(false);
-	};
-	
 	self.editorWidth = editorWidth;
 	self.editorIsVisible = ko.observable(true);
 	self.editorIsHidden = ko.computed(function() {

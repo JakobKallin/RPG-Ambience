b301148fc1409b6c5424e6ebd2e3760db1fc2b0a
Jakob Kallin <jakobkallin>
2013-02-28 16:08:12 +0100

    2013 license

--- source/Scene.Text.js
+++ source/Scene.Text.js
@@ -1,5 +1,5 @@
 // This file is part of RPG Ambience
-// Copyright 2012 Jakob Kallin
+// Copyright 2012-2013 Jakob Kallin
 // License: GNU GPL (http://www.gnu.org/licenses/gpl-3.0.txt)
 
 Ambience.App.Scene.Text = function() {

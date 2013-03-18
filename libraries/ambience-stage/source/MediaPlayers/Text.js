d37830f4a38cc64b4f6915e5bb0e32d26a55dc73
Jakob Kallin <jakobkallin>
2013-03-18 19:03:26 +0100

    Copyright year

--- libraries/ambience-stage/source/MediaPlayers/Text.js
+++ libraries/ambience-stage/source/MediaPlayers/Text.js
@@ -1,5 +1,5 @@
 // This file is part of Ambience Stage
-// Copyright 2012 Jakob Kallin
+// Copyright 2012-2013 Jakob Kallin
 // License: GNU GPL (http://www.gnu.org/licenses/gpl-3.0.txt)
 
 Ambience.Text = function(container) {

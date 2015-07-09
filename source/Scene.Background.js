a41fca34ba0c3f5fc09a1c941e499052e2e5a321
Jakob Kallin <jakobkallin@gmail.com>
2015-07-09 14:42:45 +0200

    Cleanup for version 2

--- source/Scene.Background.js
+++
@@ -1,9 +0,0 @@
-// This file is part of RPG Ambience
-// Copyright 2012-2013 Jakob Kallin
-// License: GNU GPL (http://www.gnu.org/licenses/gpl-3.0.txt)
-
-Ambience.App.Scene.Background = function() {
-	return {
-		color: '#000000'
-	};
-}
